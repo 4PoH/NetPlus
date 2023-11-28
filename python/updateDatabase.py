5171ab3981f0d19472247a9dcc8e395785da794d
Anass Hmadouch <hmadouchanass@gmail.com>
2023-11-28 11:11:41 +0100

    Ajout de scripts

--- python/updateDatabase.py
+++ python/updateDatabase.py
@@ -1,78 +1,55 @@
-import csv
+#Créer les fichiers csv des séries selon leur fichier texte (txt)
 import os
-import json
-from fuzzywuzzy import fuzz # Permet de trouver des correspondance
-import pymongo # Permet de d'interagire avec une base de données Mongo
-import pandas as pd
-from tqdm import tqdm # Affiche une barre de progression dans le terminal lors de l'execution du script 
+import re
+import csv
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-# Permet de trouver les correspondances entre le nom des répertoires de séries 
-# avec une liste de nom de séries écrit en "propre"
-def trouver_correspondance(terme_a_comparer, liste_de_series):
-    correspondance_trouvee = None
-    meilleur_score = 0
+def decoupe(texte):
+    texte_decoupe = list(re.split('; |, |\' |\n |\s+', texte))
+    return " ".join(texte_decoupe) 
 
-    for nom_serie in liste_de_series:
-        score = fuzz.ratio(terme_a_comparer.lower(), nom_serie.lower())
-        if score > meilleur_score:
-            meilleur_score = score
-            correspondance_trouvee = nom_serie
+def lire_fichier(nom_fichier):
+    try:
+        with open(nom_fichier, 'r', encoding='ansi') as fichier:
+            contenu = fichier.read()
+        return contenu
+    except FileNotFoundError:
+        print("Erreur")
+        return ""
 
-    return correspondance_trouvee
+def creer_csv_series(dossier_series, dossier_sortie):
+    if not os.path.exists(dossier_sortie):
+        os.makedirs(dossier_sortie)
 
-def creer_dictionnaire_mots_cles(repertoire_series, chemin_fichier_csv):
-    dictionnaire_mots_cles = {}
-    colonne_propre = []
+    for nom_fichier in os.listdir(dossier_series):
+        chemin_fichier = os.path.join(dossier_series, nom_fichier)
+        contenu = lire_fichier(chemin_fichier)
+        
+        if contenu:
+            resultat = decoupe(contenu) 
+            vectorizer = TfidfVectorizer()
+            tfidf_matrix = vectorizer.fit_transform([resultat])
+            feature_names = vectorizer.get_feature_names_out()
+            tfidf_scores = tfidf_matrix.toarray()[0]
+            mots_cles_pondere = list(zip(feature_names, tfidf_scores))
+            mots_cles_pondere = sorted(mots_cles_pondere, key=lambda x: x[1], reverse=True)
+            
+            nom_sortie = os.path.splitext(nom_fichier)[0] + ".csv"
+            chemin_sortie = os.path.join(dossier_sortie, nom_sortie)
+            
+            # limite_mots = 50000
 
-    with open(chemin_fichier_csv, "r") as fichier_csv:
-        lecteur_csv = csv.reader(fichier_csv)
-        for ligne in lecteur_csv:
-            if len(ligne) >= 2:
-                colonne_propre.append(ligne[1])
+            with open(chemin_sortie, "w", newline='') as fichier_csv:
+                writer = csv.writer(fichier_csv)
+                writer.writerow(["Mot", "Poids"])  
+                
+                mots_inclus = 0 
+                
+                for mot, poids in mots_cles_pondere:
+                    # if mots_inclus < limite_mots:
+                    writer.writerow([mot, poids])
+                    mots_inclus += 1 
 
-    for nom_fichier in os.listdir(repertoire_series):
-        if nom_fichier.endswith(".csv"):
-            chemin_fichier = os.path.join(repertoire_series, nom_fichier)
-            with open(chemin_fichier, "r", newline='', encoding="ansi") as fichier_csv:
-                lecteur_csv = csv.reader(fichier_csv)
-                next(lecteur_csv, None) 
-
-                nom_serie_propre = trouver_correspondance(nom_fichier[:-4], colonne_propre) or "Nom_inconnu"
-
-                for ligne in lecteur_csv:
-                    if len(ligne) >= 2:
-                        mot = ligne[0]
-                        poids = float(ligne[1])
-
-                        if mot in dictionnaire_mots_cles:
-                            dictionnaire_mots_cles[mot]["liste_series"].append({"serie": nom_serie_propre, "poids": poids})
-                        else:
-                            dictionnaire_mots_cles[mot] = {"liste_series": [{"serie": nom_serie_propre, "poids": poids}]}
-
-    return dictionnaire_mots_cles
-
-def importer_donnees_mongo(dictionnaire_mots_cles):
-    client = pymongo.MongoClient("mongodb://localhost:27017/") 
-    database = client["SAE_C01"]
-    collection = database["séries"]
-    
-    barre_progression = tqdm(dictionnaire_mots_cles.items(), desc="Importation des données", unit="mot")
-    
-    for mot, details in barre_progression:
-        jsonFormat = {"Mot": str(mot), "liste_series": details["liste_series"]}
-        collection.insert_one(jsonFormat)
-    
-    client.close()
-
-repertoire_series = "script/listes_mots_series"
-chemin_fichier_csv = "script/liste_dossiers.csv"
-
-dictionnaire_mots_cles = creer_dictionnaire_mots_cles(repertoire_series, chemin_fichier_csv)
-chemin_fichier_json = "mots_cles.json"
-
-with open(chemin_fichier_json, "w", encoding="utf-8") as fichier_json:
-    json.dump(dictionnaire_mots_cles, fichier_json, indent=4)
-
-importer_donnees_mongo(dictionnaire_mots_cles)
-
-print(f"Le dictionnaire des mots-clés a été importé dans la collection MongoDB.")
+dossier_series = "series"
+dossier_sortie = "listes_mots_series"
+creer_csv_series(dossier_series, dossier_sortie)
